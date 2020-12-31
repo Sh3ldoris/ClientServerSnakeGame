0b633576b308912d958c189cbf4ecd82af687a07
lanya <adam.lany@posam.sk>
2020-12-31 14:58:52 +0100

    Added curses library

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-#include <termios.h>
-
+#include <curses.h>
 
 /// Rozmery plochy
 #define N  50
-#define M  15
+#define M  18
 
 int field[M][N] = {0}; /// Pozicia pre hraca 1
 int direction = 2; /// Smer pohybu (1-4 clockwise)
@@ -24,60 +23,114 @@ int fruit_value = 0;
 
 int play = 0;
 
-void set_mode(int want_key)
-{
-    static struct termios old, new;
-    if (!want_key) {
-        tcsetattr(STDIN_FILENO, TCSANOW, &old);
-        return;
-    }
+void draw_area();
+int key_hit();
+void draw_game();
+void snake_init();
+void draw_area();
+void draw_game_over();
+void generate_fruit();
+void eat_fruit();
+void check_collision();
+void step(int change);
+void start_screen();
+void loser_screen();
+void winner_screen();
 
-    tcgetattr(STDIN_FILENO, &old);
-    new = old;
-    new.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &new);
-}
+int main() {
 
-int get_key()
-{
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
+
+    srand(time(NULL));
     int c = 0;
-    struct timeval tv;
-    fd_set fs;
-    tv.tv_usec = tv.tv_sec = 0;
+    int direction_change = 2;
 
-    FD_ZERO(&fs);
-    FD_SET(STDIN_FILENO, &fs);
-    select(STDIN_FILENO + 1, &fs, 0, 0, &tv);
+    start_screen();
 
-    if (FD_ISSET(STDIN_FILENO, &fs)) {
-        c = getchar();
-        set_mode(0);
+    snake_init();
+    draw_area();
+
+    while(play) {
+        /// Generate fruit
+        if (fruit_generated == 0) {
+            generate_fruit();
+        }
+
+        /// Print game area
+        draw_game();
+
+
+        /// Get input
+        if (key_hit()) {
+            c = getch();
+            if (c == 97)
+                direction_change = 4;
+            if (c == 100)
+                direction_change = 2;
+            if (c == 120)
+                play = 0;
+            if (c == 119)
+                direction_change = 1;
+            if (c == 115)
+                direction_change = 3;
+        }
+
+        /// Snake step
+        step(direction_change);
+
+        usleep(300000);
     }
-    return c;
+    refresh();
+
+    draw_game_over();
+
+    sleep(2);
+    system("clear");
+
+    if (current_score == 0)
+        loser_screen();
+    if (current_score > 0)
+        winner_screen();
+
+
+    endwin();
+    return 0;
 }
 
-void print() {
-    for(int i=0;i<=M;i++){
-        for (int j = 0; j <= N; ++j) {
-            if (i == 0 || i == M || j == 0 || j == N) {
-                printf("#");
-            } else if ((field[i][j] >= tail) && (field[i][j] < head)) {
-                printf("o");
+
+int key_hit() {
+    int ch = getch();
+
+    if (ch != ERR) {
+        ungetch(ch);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void draw_game() {
+    for(int i = 1; i <= M - 1; i++){
+        for (int j = 1; j <= N - 1; j++) {
+             if ((field[i][j] >= tail) && (field[i][j] < head)) {
+                mvprintw(i, j, "o");
             } else if (field[i][j] == head) {
-                printf("x");
+                 mvprintw(i, j, "x");
             } else if (fruit_generated == 1 && j == fruit_x && i == fruit_y) {
-                printf("%d",fruit_value);
+                 mvprintw(i, j, "%d",fruit_value);
             } else {
-                printf(" ");
+                 mvprintw(i, j, " ");
             }
             /// !!! Bacha na ELSE vetvu !!!
         }
-        printf("\n");
     }
-
-
-    printf("\n   Current Score: %d  HighScore: %d",current_score, 100);
-    printf("\n");
+    mvprintw(M + 2, (N/2) - 16, "Current Score: %d  HighScore: %d",current_score, 100);
+    move(M + 3, 0);
+    refresh();
 }
 
 
@@ -89,6 +142,30 @@ void snake_init() {
     for (int i = 0; i < head; ++i) {
         field[y][++j - head] = i + 1;
     }
+}
+
+void draw_area() {
+    move(0,0);
+    for(int i=0;i<=M;i++){
+        for (int j = 0; j <= N; ++j) {
+            if (i == 0 || i == M || j == 0 || j == N) {
+                printw("#");
+            } else {
+                printw(" ");
+            }
+            /// !!! Bacha na ELSE vetvu !!!
+        }
+        printw("\n");
+    }
+    refresh();
+}
+
+void draw_game_over() {
+    mvprintw((M / 2) - 1, (N/2) - 6, "           ");
+    mvprintw(M / 2, (N/2) - 6, " Game OVER ");
+    mvprintw((M / 2) + 1, (N/2) - 6, "           ");
+    move(0,0);
+    refresh();
 }
 
 void generate_fruit() {
@@ -190,121 +267,68 @@ void step(int change) {
 
 void start_screen() {
     system("clear");
-    printf("\n");
-    printf("    ________         ________ 			\n");
-    printf("   /        \\       /        \\ 			\n");
-    printf("  /  /----\\  \\     /  /----\\  \\ 			\n");
-    printf("  |  |    |  |     |  |    |  |       /	\n");
-    printf("  |  |    |  |     |  |    |  |      /|	\n");
-    printf(" (o  o)    \\  \\____/  /    \\  \\_____/ | 	\n");
-    printf("  \\__/      \\        /      \\        / 	\n");
-    printf("             --------        -------- 		\n");
-    printf("    											\n");
-    printf("	    Start when you're ready!\n");
-    printf("	    Press ENTER to START.	\n");
+    move(0,0);
+    printw("\n");
+    printw("    ________         ________ 			  \n");
+    printw("   /        \\       /        \\        0  \n");
+    printw("  /  /----\\  \\     /  /----\\  \\       0  \n");
+    printw("  |  |    |  |     |  |    |  |      / \\ \n");
+    printw("  |  |    |  |     |  |    |  |     /  |  \n");
+    printw(" (o  o)   \\   \\___/   /    \\   \\___/   / \n");
+    printw("  \\__/     \\         /      \\         /  \n");
+    printw("            --------         -------- 	   \n");
+    printw("\n");
+    printw("	Start when you are ready!\n");
+    printw("	  Press ENTER to START.	\n");
 
-    printf("\n");
+    refresh();
 
-    int c = fgetc(stdin);
-        if (c == 10)
-            play = 1;
+    while (getch() != '\n')  /// caka na enter
+    play = 1;
+    system("clear");
 }
 
 void loser_screen() {
     system("clear");
-    printf("\n");
-    printf("         ________    		\n");
-    printf("        /        \\     		\n");
-    printf("       /  /----\\  \\   		\n");
-    printf("       |  |    |  |    	\n");
-    printf("       |  |    |  |   	\n");
-    printf("      (o  o)   |  | 	\n");
-    printf("  |\\   \\__/    |  | 	\n");
-    printf("  | \\__________/  /    	\n");
-    printf("  \\              /    	\n");
-    printf("   -------------  		\n");
-    printf("	    Game OVER!\n");
-    printf("	    Your SCORE: %d !\n", current_score);
-    printf("\n");
+    move(0,0);
+    printw("\n");
+    printw("         ________    		\n");
+    printw("        /        \\     		\n");
+    printw("       /  /----\\  \\   		\n");
+    printw("       |  |    |  |    	\n");
+    printw("       |  |    |  |   	\n");
+    printw("      (o  o)   |  | 	\n");
+    printw("  |\\   \\__/    |  | 	\n");
+    printw("  | \\__________/  /    	\n");
+    printw("  \\              /    	\n");
+    printw("   -------------  		\n");
+    printw("	      Game OVER!\n");
+    printw("	    Your SCORE: %d !\n", current_score);
+    printw("\n");
+    refresh();
 
+    while (getch() != '\n');
 }
 
 void winner_screen() {
     system("clear");
-    printf("\n");
-    printf("         _________________________________    		\n");
-    printf("        /                                 \\     		\n");
-    printf("       /  /-----------------------------\\  \\   		\n");
-    printf("       |  |          . . . . . . .      |  |    	\n");
-    printf("       |  |          |\\/\\/\\/\\/\\/\\|      |  |   	\n");
-    printf("      (o  o)         | o o o o o |      |  | 	\n");
-    printf("       \\__/    |\\    |___________|      |  | 	\n");
-    printf("               | \\______________________/  /    	\n");
-    printf("               \\                          /    	\n");
-    printf("                --------------------------  		\n");
-    printf("	    You WON!\n");
-    printf("	    Your SCORE: %d !\n", current_score);
+    move(0,0);
+    printw("\n");
+    printw("         _________________________________    		\n");
+    printw("        /                                 \\     		\n");
+    printw("       /  /-----------------------------\\  \\   		\n");
+    printw("       |  |          . . . . . . .      |  |    	\n");
+    printw("       |  |          |\\/\\/\\/\\/\\/\\|      |  |   	\n");
+    printw("      (o  o)         | o o o o o |      |  | 	\n");
+    printw("       \\__/    |\\    |___________|      |  | 	\n");
+    printw("               | \\______________________/  /    	\n");
+    printw("               \\                          /    	\n");
+    printw("                --------------------------  		\n");
+    printw("	       You WON!\n");
+    printw("	    Your SCORE: %d !\n", current_score);
 
-    printf("\n");
+    printw("\n");
+    refresh();
+    while (getch() != '\n');
 
-}
-
-
-int main() {
-    srand(time(NULL));
-    int c = 0;
-    int direction_change = 2;
-
-
-    start_screen();
-
-    snake_init();
-
-    while(play) {
-        /// Clear windows
-        system("clear");
-
-        /// Generate fruit
-        if (fruit_generated == 0) {
-            generate_fruit();
-        }
-
-        /// Print game
-        print();
-
-        printf("Direction --> %d\n", direction);
-        printf("Head x,y--> [%d,%d]\n", x, y);
-        printf("Fruit x,y--> [%d,%d]\n", fruit_x, fruit_y);
-        printf("Tail --> %d\n", tail);
-        printf("Body --> %d\n", head - tail);
-
-        /// Get input
-        set_mode(1);
-        c = get_key();
-        if (c != 0) {
-            if (c == 97)
-                direction_change = 4;
-            if (c == 100)
-                direction_change = 2;
-            if (c == 120)
-                play = 0;
-            if (c == 119)
-                direction_change = 1;
-            if (c == 115)
-                direction_change = 3;
-        }
-
-        /// Snake step
-        step(direction_change);
-
-        usleep(300000);
-    }
-
-    if (current_score == 0)
-        loser_screen();
-    if (current_score > 0)
-        winner_screen();
-
-
-    return 0;
 }
